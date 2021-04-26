29a2bdb5bc604e4b483e4fe0873ab81c4c5df7bd
R.J <rjschodowski@hotmail.com>
2021-04-26 14:47:20 -0400

    commit on 'main'

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,20 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Head from '../components/head'
-// import Profileimage from '../images/profilephoto.jpg'
+import { StaticImage } from 'gatsby-plugin-image'
+
 
 const IndexPage = () => {
   return (
     <Layout>
       <Head title="Home"/>
-      {/* <Profileimage /> */}
+      <StaticImage
+      src="../images/profilephoto.jpg"
+      alt="profile photo of R.J. Schodowski"
+      placeholder="blurred"
+      layout="fixed"
+      width={200}
+      height={200} />
       <h1>Hi! I'm R.J.</h1>
       <p>I am a social-minded web developer who gets major fulfillment in coding projects that positively impact people from all walks of life.</p>
       <Link to="/about">More about me</Link>
