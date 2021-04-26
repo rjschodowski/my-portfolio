82ef03548131dfed745151e4f2f599f9a91c8147
R.J <rjschodowski@hotmail.com>
2021-04-26 15:23:24 -0400

    commit on 'main'

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Head from '../components/head'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
 const Contact = () => {
     return (
         <Layout>
             <Head title="Contact"/>
             <h1>Contact</h1>
-            <p>Reach me <a href="https://twitter.com/RJSchodowski" target="_blank">@RJSchodowski</a> on Twitter!</p>
+            <form className={classes.root} noValidate autoComplete="off">
+                <TextField id="standard-basic" label="Standard" />
+                <TextField id="filled-basic" label="Filled" variant="filled" />
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            </form>
         </Layout>
     )
 }
