99beebf0d03c5da73f322eb67d6beb754e5072cf
R.J <rjschodowski@hotmail.com>
2021-05-20 16:43:34 -0400

    rel="noreferrer" added to external links in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,9 +16,9 @@ const Footer = () => {
     return (
         <div className={footer}>
             <ul>
-                <li><a href="https://github.com/rjschodowski" alt="R.J. Schodowski GitHub profile" target="_blank">github</a></li>
-                <li><a href="https://www.linkedin.com/in/rjschodowski/" alt="R.J. Schodowski LinkedIn profile" target="_blank">linkedin</a></li>
-                <li><a href="https://twitter.com/RJSchodowski" alt="R.J. Schodowski Twitter profile" target="_blank">twitter</a></li>
+                <li><a href="https://github.com/rjschodowski" alt="R.J. Schodowski GitHub profile" target="_blank" rel="noreferrer">github</a></li>
+                <li><a href="https://www.linkedin.com/in/rjschodowski/" alt="R.J. Schodowski LinkedIn profile" target="_blank" rel="noreferrer">linkedin</a></li>
+                <li><a href="https://twitter.com/RJSchodowski" alt="R.J. Schodowski Twitter profile" target="_blank" rel="noreferrer">twitter</a></li>
             </ul>
             <p>Created by {data.site.siteMetadata.author}, &#169; 2021</p>
         </div>
