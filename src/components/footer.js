197ad2e65e462c2002659080cbb5f252217e2ad0
R.J <rjschodowski@hotmail.com>
2021-05-20 16:46:01 -0400

    erased unused imported Link and link in header and footer components

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, graphql, useStaticQuery } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import { footer } from './footer.module.css'
 
 const Footer = () => {
