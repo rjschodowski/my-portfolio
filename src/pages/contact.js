6387031fd469c2de17ac41a772bc8e73d787d73b
R.J <rjschodowski@hotmail.com>
2021-06-11 14:56:38 -0400

    contact form width size increased

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,7 @@ const useStyles = makeStyles((theme) => ({
     root: {
       '& > *': {
         margin: theme.spacing(1),
-        width: '25ch',
+        width: '40ch',
         marginLeft: 0,
       },
     },
