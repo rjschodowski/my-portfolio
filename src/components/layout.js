611a2fe58afceb60954ffcc6432c0ea0393a0e73
R.J <rjschodowski@hotmail.com>
2021-06-17 15:00:51 -0400

    commit on 'main'

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,12 @@ import Header from './header'
 import Footer from './footer'
 import '../styles/global.css'
 import { container, content } from './layout.module.css'
-import '../styles/bg-image.css'
+import GbiBridged from './bg-image'
 
 const Layout = (props) => {
     return (
         <div className={container}>
+            <GbiBridged />
             <div className={content}>
                 <Header />
                 {props.children}  
