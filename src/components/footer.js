52db5c0d6b5e69ca220984b1ffdffe204afbc9c1
R.J <rjschodowski@hotmail.com>
2021-06-19 10:23:13 -0400

    styling changed in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
+import { navItem } from './footer.module.css'
 
 const Footer = () => {
     const data = useStaticQuery(graphql`
@@ -16,9 +17,9 @@ const Footer = () => {
         <nav className="flex justify-between mt-12">
             <p className="mb-1">&#169; 2021 {data.site.siteMetadata.author}</p>
             <ul className="flex m-0 p-0">
-                <li><a href="https://github.com/rjschodowski" alt="R.J. Schodowski GitHub profile" target="_blank" rel="noreferrer">github</a></li>
-                <li className="px-2"><a href="https://www.linkedin.com/in/rjschodowski/" alt="R.J. Schodowski LinkedIn profile" target="_blank" rel="noreferrer">linkedin</a></li>
-                <li className=""><a href="https://twitter.com/RJSchodowski" alt="R.J. Schodowski Twitter profile" target="_blank" rel="noreferrer">twitter</a></li>
+                <li className={navItem}><a href="https://github.com/rjschodowski" alt="R.J. Schodowski GitHub profile" target="_blank" rel="noreferrer">github</a></li>
+                <li className={navItem}><a href="https://www.linkedin.com/in/rjschodowski/" alt="R.J. Schodowski LinkedIn profile" target="_blank" rel="noreferrer">linkedin</a></li>
+                <li className={navItem}><a href="https://twitter.com/RJSchodowski" alt="R.J. Schodowski Twitter profile" target="_blank" rel="noreferrer">twitter</a></li>
             </ul>
             
         </nav>
