23c5d588e83db89e897b8a7e7888575565149949
R.J <rjschodowski@hotmail.com>
2021-06-22 15:16:07 -0400

    form subission configured on contact.js page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,40 +7,40 @@ import Button from '@material-ui/core/Button';
 import { heading, contactContainer } from '../styles/contact.module.css'
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: '40ch',
-        marginLeft: 0,
-      },
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '40ch',
+      marginLeft: 0,
     },
-  }));
+  },
+}));
 
 export default function BasicTextFields() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-            <Layout>
-                <SEO title="Contact"/>
-                <div className={contactContainer}>
-                  <h1 className={heading}>Contact</h1>
-                  <div>
-                    <form className={classes.root} noValidate autoComplete="off">
-                        <TextField id="standard-basic" label="Name" />
-                        <TextField id="standard-basic" label="Subject" />
-                        <TextField id="standard-basic" label="Email Address" />
-                        <TextField
-                            id="standard-multiline-static"
-                            label="Messages"
-                            multiline
-                            rows={5}
-                            />
-                    </form>
-                    <Button variant="outlined" color="primary">Submit</Button>
-                  </div>
-                </div>
-            </Layout>
-    )
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <div className={contactContainer}>
+        <h1 className={heading}>Contact</h1>
+        <div>
+          <form name="contact" method="POST" data-netlify="true" className={classes.root} noValidate autoComplete="off">
+            <TextField id="standard-basic" label="Name" />
+            <TextField id="standard-basic" label="Subject" />
+            <TextField id="standard-basic" label="Email Address" />
+            <TextField
+              id="standard-multiline-static"
+              label="Messages"
+              multiline
+              rows={5}
+            />
+          </form>
+          <Button variant="outlined" color="primary">Submit</Button>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 
@@ -53,10 +53,10 @@ export default function BasicTextFields() {
 //       },
 //     },
 //   }));
-  
+
 //   export default function BasicTextFields() {
 //     const classes = useStyles();
-  
+
 //     return (
 //       <form className={classes.root} noValidate autoComplete="off">
 //         <TextField id="standard-basic" label="Standard" />
