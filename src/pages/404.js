3778f1c550d08b8d45b6fd4ec4dfa390ad5b2e8d
R.J <rjschodowski@hotmail.com>
2021-06-25 13:32:20 -0400

    404 page updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { link } from '../styles/404.module.css'
 
 const NotFound = () => {
     return (
         <Layout>
             <SEO title="404"/>
-            <h1>Page not found</h1>
-            <p><Link to="/">Head home</Link></p>
+            <h1>404 - Page not found</h1>
+            <p><Link to="/" className={link}>Head home</Link></p>
         </Layout>
     )
 }
