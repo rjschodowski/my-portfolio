d569d784c363bd69c78ca110129830979cd42d56
R.J <rjschodowski@hotmail.com>
2021-06-27 13:52:21 -0400

    right margin removed for social media icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import { navItem, copyright } from './footer.module.css'
-import { socialIcon } from '../styles/contact.module.css'
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import GitHubIcon from '@material-ui/icons/GitHub';
@@ -23,21 +22,21 @@ const Footer = () => {
             <ul className="md:flex m-0 p-0">
                 <li className={navItem}>
                     <a href="https://www.linkedin.com/in/rjschodowski/" alt="R.J. Schodowski LinkedIn profile" target="_blank" rel="noreferrer">
-                        <span className={socialIcon}>
+                        <span>
                             <LinkedInIcon />
                         </span>   
                     </a>
                 </li>
                 <li className={navItem}>
                     <a href="https://twitter.com/RJSchodowski" alt="R.J. Schodowski Twitter profile" target="_blank" rel="noreferrer">
-                        <span className={socialIcon}>
+                        <span>
                             <TwitterIcon />
                         </span>
                     </a>
                 </li>
                 <li className={navItem}>
                     <a href="https://github.com/rjschodowski" alt="R.J. Schodowski GitHub profile" target="_blank" rel="noreferrer">
-                        <span className={socialIcon}>
+                        <span>
                             <GitHubIcon />
                         </span>
                     </a>
