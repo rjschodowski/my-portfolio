6207ac392089c055f0cad0282f758f74689866df
R.J <rjschodowski@hotmail.com>
2021-06-27 13:27:04 -0400

    footer links' text replaced with social media icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import { navItem, copyright } from './footer.module.css'
+import { socialIcon } from '../styles/contact.module.css'
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const Footer = () => {
     const data = useStaticQuery(graphql`
@@ -17,9 +21,27 @@ const Footer = () => {
         <nav className="flex justify-between mt-12">
             <p className={copyright}>&#169; 2021 {data.site.siteMetadata.author}</p>
             <ul className="md:flex m-0 p-0">
-                <li className={navItem}><a href="https://github.com/rjschodowski" alt="R.J. Schodowski GitHub profile" target="_blank" rel="noreferrer">github</a></li>
-                <li className={navItem}><a href="https://www.linkedin.com/in/rjschodowski/" alt="R.J. Schodowski LinkedIn profile" target="_blank" rel="noreferrer">linkedin</a></li>
-                <li className={navItem}><a href="https://twitter.com/RJSchodowski" alt="R.J. Schodowski Twitter profile" target="_blank" rel="noreferrer">twitter</a></li>
+                <li className={navItem}>
+                    <a href="https://www.linkedin.com/in/rjschodowski/" alt="R.J. Schodowski LinkedIn profile" target="_blank" rel="noreferrer">
+                        <span className={socialIcon}>
+                            <LinkedInIcon />
+                        </span>   
+                    </a>
+                </li>
+                <li className={navItem}>
+                    <a href="https://twitter.com/RJSchodowski" alt="R.J. Schodowski Twitter profile" target="_blank" rel="noreferrer">
+                        <span className={socialIcon}>
+                            <TwitterIcon />
+                        </span>
+                    </a>
+                </li>
+                <li className={navItem}>
+                    <a href="https://github.com/rjschodowski" alt="R.J. Schodowski GitHub profile" target="_blank" rel="noreferrer">
+                        <span className={socialIcon}>
+                            <GitHubIcon />
+                        </span>
+                    </a>
+                </li>
             </ul>
 
         </nav>
