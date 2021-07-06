3ec795f16e78eae7b53830eabcc79852697c9a46
R.J <rjschodowski@hotmail.com>
2021-07-06 11:37:07 -0400

    navigation link added to form submitted thanks.js page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import { link } from '../styles/404.module.css'
 import Layout from '../components/layout'
 
 export default () => (
   <Layout>
     <h1>Thank you!</h1>
     <p>I will get back with you as quickly as possible.</p>
+    <p><Link to="/" className={link}>Head home</Link></p>
   </Layout>
 )
