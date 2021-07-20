08670f0e4cddb39b5ff588bb1e09ff9ed7b7c5e4
R.J <rjschodowski@hotmail.com>
2021-07-20 17:48:32 -0400

    Thanks.js page refactored

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -3,10 +3,12 @@ import { Link } from 'gatsby'
 import { link } from '../styles/404.module.css'
 import Layout from '../components/layout'
 
-export default () => (
+const Thanks = () => (
   <Layout>
     <h1>Thank you!</h1>
     <p>I will get back with you as quickly as possible.</p>
     <p><Link to="/" className={link}>Head home</Link></p>
   </Layout>
 )
+
+export default Thanks
