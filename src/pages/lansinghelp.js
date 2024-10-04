0d5c172e14c4a9c25e9aacd7eec1f78018097839
Schodowski RJ (XC-AN/ENG2-NA) <RJ.Schodowski@us.bosch.com>
2024-10-04 08:52:20 -0400

    fixing CSS headline class addition bug from previous commit.

--- src/pages/lansinghelp.js
+++ src/pages/lansinghelp.js
@@ -11,7 +11,7 @@ const LansingHelp = () => {
             <SEO title="Lansing Help Guide"/>
             <div className={contentWrapper}>
                 <h1 className={heading}>Lansing Help Guide</h1>
-                <p className={`${styles.textDescription} ${styles.headline}`}>A quick starting point 
+                <p className={`${textDescription} ${headline}`}>A quick starting point 
                     for Lansing newcomers in accessing help in times of need.
                 </p>
                 <p className={textDescription}>Lansing Help Guide is a React progressive web application, which was built using
